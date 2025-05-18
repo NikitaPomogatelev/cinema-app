5ec1efcbd3d95e23b6ef296ab210162ad8315974
Nikita Pomogatelev <nikitoshelper@mail.ru>
2025-05-18 15:54:24 +0500

    first init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+
+
+function App() {
+
+	return (
+		<div className='site-container'>
+			<header>Это шапка</header>
+			<main className='main'>
+				<div className="container">
+					<h1>Cinema search</h1>
+
+				</div>
+
+			</main>
+			<footer>Это подвал</footer>
+    
+       
+		</div>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import 'normalize.css';
+import './assets/styles/reset.css';
+import './assets/styles/index.css';
+
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
