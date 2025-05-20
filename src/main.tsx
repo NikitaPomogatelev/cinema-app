0e0ab79a2be737272e128b4ed9b0631f5240fc8b
Nikita Pomogatelev <nikitoshelper@mail.ru>
2025-05-20 21:18:34 +0500

    Вынес роуты в отдельный файл

--- src/main.tsx
+++ src/main.tsx
@@ -5,61 +5,10 @@ import 'normalize.css';
 import './assets/styles/reset.css';
 import './assets/styles/index.css';
 
-import Auth from './components/pages/Auth/Auth.tsx';
-import Search from './components/pages/Search/Search.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import Favorite from './components/pages/Favorite/Favorite.tsx';
-import Layout from './layout/Layout.tsx';
-import Product from './components/pages/Product/Product.tsx';
+import { RouterProvider } from 'react-router';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <Search /> 
-			},
-			{
-				path: 'favorite',
-				element: <Favorite /> 
-			},
-			{
-				path: '/product/:id',
-				element: <Product />,
-				errorElement: <>Ошибка</>,
-				loader: async ({params}) => {
-					await new Promise<void>((resolve) => {
-						setTimeout(() => {
-							resolve();
-							console.log('resolve');
-					
-						}, 1000);
-					});
-					// const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+import { router } from './router/routes';
 
-					// return data;
-				}
-			},
-		]
-		
-	},
-	{
-		path: '/auth',
-		element: <Layout />,
-		children: [
-			{
-				path: 'login',
-				element: <Auth /> 
-			}
-		]
-	}
-	// {
-	// 	path: '*',
-	// 	element: <Error />
-	// }
-]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
