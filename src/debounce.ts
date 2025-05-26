8276cb31bee1f892b64441248177caa283209656
Nikita Pomogatelev <nikitoshelper@mail.ru>
2025-05-27 00:01:59 +0500

    Правка по типизации

---
+++ src/debounce.ts
@@ -0,0 +1,11 @@
+
+export function debounce<T extends (...args: unknown[]) => void>(fn: T, delay: number) {
+	let timeoutId: ReturnType<typeof setTimeout> | null;
+
+	return (...args: Parameters<T>) => {
+		if (timeoutId) clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			fn(...args);
+		}, delay);
+	};
+}
